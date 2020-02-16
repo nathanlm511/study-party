e971571f02b9b38d4ca3a311f8b87716ee67a4d8
gulnazsayed <gulnaz@vt.edu>
2020-02-16 03:19:09 -0500

    added checkbox not working

--- study-party/src/Toolbar.js
+++ study-party/src/Toolbar.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
 import { faUserCircle, faUsers, faCog  } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Checkbox from '@material-ui/core/Checkbox';
+import CircleCheckedFilled from '@material-ui/icons/CheckCircle';
+import CircleUnchecked from '@material-ui/icons/RadioButtonUnchecked';
 import './Toolbar.css';
 
 const name = "Gulnaz" //pass in first name from database
-const colors = ["#993365", "#FF0066", "#008001", "#FE9900", "#FE5B00"] //add two colors
+const colors = ["#993365", "#FF0066", "#008001", "#FE9900", "#FE5B00"]
+var category = [];
 
 class Toolbar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            border: "black"
+        }
+    }
+
+    changeBorder = (i) => (e) => {
+        
+        if (this.state.border === colors[i]) {
+            this.setState({border: "blue"});
+            console.log(this.state.border)
+        }
+        else {
+            this.setState({border: colors[i]});
+            console.log(this.state.border)
+        }
+    }
 
     classes = ["Intro to Python", "General Chemistry", "Data Structures", "Vector Geometry", "Engineering Physics"]; //get array of classes from database
     
@@ -26,14 +48,16 @@ class Toolbar extends Component {
                     <div className='header'>
                         <span className="span-class" style={{width: "100%", textAlign: "center", fontSize: "36px", lineHeight: "44px"}}>MY CLASSES</span> 
                     </div>
-                    {this.classes.map((str, i) => <div className="class" key = {i} style={{backgroundColor: colors[i]}}>
-                    <label htmlFor={i} className="label">
-                    <input type="checkbox" id={i} className="browser-default"/>
-                    <span></span>
-                    </label>
-                    <span className="span-class" style={{fontSize: "36px", lineHeight: "44px"}}>{str}</span>                     
-                        
-                        </div>)}
+                    {this.classes.map((str, i) => 
+                       <div className="class" key = {i} style={{backgroundColor: colors[i], borderColor: this.state.border, borderWidth: "5px"}} onClick={this.changeBorder(i)}>
+                            <Checkbox
+                                icon={<CircleUnchecked />}
+                                checkedIcon={<CircleCheckedFilled />}
+                                label={str}
+                            />
+                            <span className="span-class" style={{fontSize: "36px", lineHeight: "44px"}}>{str}</span>                     
+                        </div>
+                    )}
                 </div>
                 <div className='bottom-container' style= {{alignItems: "center", padding: "36px"}}>
                     <div style={{alignItems: "center", marginBottom: "10px", textAlign: "center"}}>
